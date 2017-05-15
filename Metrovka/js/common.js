7ddb705d8793881df45eefd1b218d882126212a6
ChernenkoKM-21 <broobroo007@gmail.com>
2017-05-15 14:22:03 +0300

    Add Yurdis, Metrovka, Phototime

---
+++ Metrovka/js/common.js
@@ -0,0 +1,10 @@
+$(function() {
+
+	// function() {
+	// 	if ()
+	// 				$(".page-header__center__nav-log__log__hamburger").show()
+	// 			else
+	// 					$(".page-header__center__nav-log__log__hamburger").hide()
+
+
+});
