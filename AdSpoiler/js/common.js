8a9a7ba628f23e86916c1783b53b3561030ed62b
ChernenkoKM-21 <broobroo007@gmail.com>
2017-06-20 19:33:10 +0300

    Add Helvetica font

--- AdSpoiler/js/common.js
+++ AdSpoiler/js/common.js
@@ -166,9 +166,9 @@ $(function() {
 			}]
 		});
 
-		///////////////////////Column with drilldown
-		// Create the chart
-		Highcharts.chart('barChart', {
+
+		/////////////FIRST BARCHART
+		Highcharts.chart('barChart1', {
 			chart: {
 				type: 'column',
 				width: 100,
@@ -231,6 +231,262 @@ $(function() {
 							name: 'Opera',
 							y: 10.91,
 							drilldown: 'Opera'
+						}]
+					}],
+					drilldown: {
+						series: [{
+							name: 'Microsoft Internet Explorer',
+							id: 'Microsoft Internet Explorer',
+							data: [
+							[
+							'v11.0',
+							24.13
+							],
+							[
+							'v8.0',
+							17.2
+							],
+							[
+							'v9.0',
+							8.11
+							],
+							[
+							'v10.0',
+							5.33
+							],
+							[
+							'v6.0',
+							1.06
+							],
+							[
+							'v7.0',
+							0.5
+							]
+							]
+						}, {
+							name: 'Chrome',
+							id: 'Chrome',
+							data: [
+							[
+							'v40.0',
+							5
+							],
+							[
+							'v41.0',
+							4.32
+							],
+							[
+							'v42.0',
+							3.68
+							],
+							[
+							'v39.0',
+							2.96
+							],
+							[
+							'v36.0',
+							2.53
+							],
+							[
+							'v43.0',
+							1.45
+							],
+							[
+							'v31.0',
+							1.24
+							],
+							[
+							'v35.0',
+							0.85
+							],
+							[
+							'v38.0',
+							0.6
+							],
+							[
+							'v32.0',
+							0.55
+							],
+							[
+							'v37.0',
+							0.38
+							],
+							[
+							'v33.0',
+							0.19
+							],
+							[
+							'v34.0',
+							0.14
+							],
+							[
+							'v30.0',
+							0.14
+							]
+							]
+						}, {
+							name: 'Firefox',
+							id: 'Firefox',
+							data: [
+							[
+							'v35',
+							2.76
+							],
+							[
+							'v36',
+							2.32
+							],
+							[
+							'v37',
+							2.31
+							],
+							[
+							'v34',
+							1.27
+							],
+							[
+							'v38',
+							1.02
+							],
+							[
+							'v31',
+							0.33
+							],
+							[
+							'v33',
+							0.22
+							],
+							[
+							'v32',
+							0.15
+							]
+							]
+						}, {
+							name: 'Safari',
+							id: 'Safari',
+							data: [
+							[
+							'v8.0',
+							2.56
+							],
+							[
+							'v7.1',
+							0.77
+							],
+							[
+							'v5.1',
+							0.42
+							],
+							[
+							'v5.0',
+							0.3
+							],
+							[
+							'v6.1',
+							0.29
+							],
+							[
+							'v7.0',
+							0.26
+							],
+							[
+							'v6.2',
+							0.17
+							]
+							]
+						}, {
+							name: 'Opera',
+							id: 'Opera',
+							data: [
+							[
+							'v12.x',
+							0.34
+							],
+							[
+							'v28',
+							0.24
+							],
+							[
+							'v27',
+							0.17
+							],
+							[
+							'v29',
+							0.16
+							]
+							]
+						}]
+					}
+				});
+
+		/////////////SECOND BARCHART
+		Highcharts.chart('barChart2', {
+			chart: {
+				type: 'column',
+				width: 100,
+				height: 70,
+			},
+			title: {
+				text: null
+			},
+			// subtitle: {
+				// 	text: 'Click the columns to view versions. Source: <a href="http://netmarketshare.com">netmarketshare.com</a>.'
+				// },
+				xAxis: {
+					type: ''
+				},
+				yAxis: {
+					title: {
+						text: ''
+					}
+
+				},
+				legend: {
+					enabled: false
+				},
+				plotOptions: {
+					series: {
+						borderWidth: 0,
+						// dataLabels: {
+							// 	enabled: true,
+							// 	format: '{point.y:.1f}%'
+							// }
+						}
+					},
+
+					tooltip: {
+						headerFormat: '<span style="font-size:11px">{series.name}</span><br>',
+						pointFormat: '<span style="color:{point.color}">{point.name}</span>: <b>{point.y:.2f}%</b> of total<br/>'
+					},
+
+					series: [{
+						name: 'Brands',
+						colorByPoint: true,
+						data: [{
+							name: 'Microsoft Internet Explorer',
+							y: 76.33,
+							drilldown: 'Microsoft Internet Explorer',
+							color: '#aaf'
+						}, {
+							name: 'Chrome',
+							y: 64.03,
+							drilldown: 'Chrome',
+							color: '#aa1985'
+						}, {
+							name: 'Firefox',
+							y: 20.38,
+							drilldown: 'Firefox',
+							color: '#aaaaaa'
+						}, {
+							name: 'Safari',
+							y: 14.77,
+							drilldown: 'Safari',
+							color: '#777777'
+						}, {
+							name: 'Opera',
+							y: 10.91,
+							drilldown: 'Opera',
+							color: '#ddd'
 						}]
 					}],
 					drilldown: {
