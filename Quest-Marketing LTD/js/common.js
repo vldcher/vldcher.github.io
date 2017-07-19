5afd213b92aaa25d539af3142e986879538f7fc6
ChernenkoKM-21 <broobroo007@gmail.com>
2017-07-19 16:49:39 +0300

    Quest-Marketing test task

---
+++ Quest-Marketing LTD/js/common.js
@@ -0,0 +1,17 @@
+$(function() {
+
+	// 	$(document).ready(function () {setInterval(function(){
+
+		// 		  $('#bonusImg>span').addClass('blink_on');
+
+		// 		  setTimeout(function(){$('#bonusImg>span').removeClass('blink_on')},1500);
+
+		// 	},1000)
+
+		// });
+		setInterval(function()
+		{
+			$("#joinButton").toggleClass("joinGold");
+		},1000);
+
+	});
